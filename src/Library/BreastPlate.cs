f061a6ce07bf336e71b800e429c7b6cdd634c268
ChristopherBentancor <chrisbentancorrevetria@gmail.com>
2022-04-30 17:30:29 -0300

    Arreglos de clase

--- src/Library/BreastPlate.cs
+++ src/Library/BreastPlate.cs
@@ -2,9 +2,38 @@ using System;
 
 namespace RolePlay
 {
-    public class breastPlace
+    public class BreastPlate
     {
         private string name;
         private int defense;
-    }    
+        
+        public BreastPlate(string name, int defense)
+        {
+            this.Name = name;
+            this.Defense = defense;
+        }
+        
+        public string Name
+        {   
+            get
+            {   
+                return name;   
+            }
+            set 
+            {
+                this.name=value;
+            }
+        }  
+        public int Defense
+        {   
+            get
+            {
+                return defense;
+            }
+            set
+            {
+                this.defense=value;
+            }
+        }
+    }
 }
--- src/Library/Helmet.cs
+++ src/Library/Helmet.cs
@@ -6,5 +6,33 @@ namespace RolePlay
     {
         private string name;
         private int defense;
+        
+        public Helmet(string name, int defense)
+        {
+            this.Name = name;
+            this.Defense = defense;
+        }
+        public string Name
+        {   
+            get
+            {   
+                return name;   
+            }
+            set 
+            {
+                this.name=value;
+            }
+        }  
+        public int Defense
+        {   
+            get
+            {
+                return defense;
+            }
+            set
+            {
+                this.defense=value;
+            }
+        }
     }    
 }
