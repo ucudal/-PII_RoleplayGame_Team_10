8f786a41ba3b23e326d76b781f241e7ea672c189
ChristopherBentancor <chrisbentancorrevetria@gmail.com>
2022-04-30 21:46:29 -0300

    Test

--- src/Test/Library.Test/GetTotalDefense.cs
+++ src/Test/Library.Test/GetTotalDefense.cs
@@ -11,9 +11,8 @@ namespace Test.Library
         {
             Dwarf dwarf1 = new Dwarf ("Grunge",50,100);
             Shield item1 = new Shield ("Shield");
-            dwarf1.AddElements(item1);
-            totalhealth = 200;
-            Assert.AreEqual(totalhealth,dwarf1.TotalDefense());
+            dwarf1.AddElement(item1);
+            Assert.AreEqual(150,dwarf1.TotalDefense());
             
         }
         
@@ -23,10 +22,9 @@ namespace Test.Library
             Elf elf1 = new Elf ("Amil",100,50);
             Shield item1 = new Shield ("Shield");
             BreastPlate item2 = new BreastPlate ("BreastPlate");
-            elf1.AddElements(item1);
-            elf1.AddElements(item2);
-            totalhealth = 300;
-            Assert.AreEqual(totalhealth,elf1.TotalDefense());
+            elf1.AddElement(item1);
+            elf1.AddElement(item2);
+            Assert.AreEqual(300,elf1.TotalDefense());
             
         }
         
@@ -38,8 +36,7 @@ namespace Test.Library
             Helmet item2 = new Helmet ("Helmet");
             wizard1.AddElement(item1);
             wizard1.AddElement(item2);
-            totalhealth = 350;
-            Assert.AreEqual (totalhealth,wizard1.TotalDefense());
+            Assert.AreEqual (350,wizard1.TotalDefense());
             
         }
     }
