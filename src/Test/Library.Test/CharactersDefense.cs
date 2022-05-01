4eeb8c58853fb781dab51de77949dfa00901aba6
ChristopherBentancor <chrisbentancorrevetria@gmail.com>
2022-04-30 21:12:14 -0300

    Tests

--- src/Test/Library.Test/CharactersDefense.cs
+++ src/Test/Library.Test/CharactersDefense.cs
@@ -1,55 +1,43 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using RolePlay;
 
-namespace RolePlay
+namespace Test.Library
 {
-
-
     public class GetTotalDefense
     {
-
         [Test]
         public void DwarfDefense()
         {
             Dwarf dwarf1 = new Dwarf ("Grunge",50,100);
-            Shield item1 = new Shield ();
-            dwarf1.AddElement(item1);
-            dwarf1.Thealth = dwarf1.health+dwarf1(item.health);
-            dwarf1.TotalDefense = 200;
-            Assert.AreEqual (dwarf1.Thealth,dwarf1.TotalDefense);
+            //Shield item1 = new Shield ();
+            //dwarf1.AddElement(item1);
+            Assert.AreEqual(dwarf1.TotalDefense,100);
             
         }
         
         [Test]
         public void ElfDefense()
         {
-            Elf elf1 = new Elf ("Amil",50,100);
-            Shield item1 = new Shield ();
-            BreastPlate item2 = new BreastPlate ();
-            elf1.AddElement(item1);
-            elf1.AddElement(item2);
-            elf1.Thealth = elf1.health+elf1(item.health);
-            elf1.TotalDefense = 300;
-            Assert.AreEqual (elf1.Thealth,elf1.TotalDefense);
+            Elf elf1 = new Elf ("Amil",100,50);
+            //Shield item1 = new Shield ();
+            //BreastPlate item2 = new BreastPlate ();
+            //elf1.AddElement(item1);
+            //elf1.AddElement(item2);
+            Assert.AreEqual (elf1.TotalDefense,100);
             
         }
         
         [Test]
         public void WizardDefense()
         {
-            Wizard wizard1 = new Wizard ("Amil",50,150);
-            Shield item1 = new Shield ();
-            Helmet item2 = new Helmet ();
-            wizard1.AddElement(item1);
-            wizard1.AddElement(item2);
-            wizard1.Thealth = wizard1.health+wizard1(item.health);
-            wizard1.TotalDefense = 350;
-            Assert.AreEqual (wizard1.Thealth,wizard1.TotalDefense);
+            Wizard wizard1 = new Wizard ("Adil",150,50);
+            //Shield item1 = new Shield ();
+            //Helmet item2 = new Helmet ();
+            //wizard1.AddElement(item1);
+            //wizard1.AddElement(item2);
+            Assert.AreEqual (wizard1.TotalDefense,150);
             
         }
-        
-        
-
     }
-
-
 }
